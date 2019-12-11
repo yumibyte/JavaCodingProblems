b9d597af62afff94f228642c821f05870858b423
Yumibyte <ashraigosa@gmail.com>
2019-12-11 00:14:21 -0800

    got rid of previous RecursiveFactorial file and replaced with solution to problem. Output factorial of number

---
+++ RecursiveFactorial1/src/RecursiveFactorial1/main.java
@@ -0,0 +1,28 @@
+package RecursiveFactorial1;
+
+public class main {
+
+    public static double total = 1;
+
+    public static double calculate(double a) {
+        System.out.println("running! " + a);
+        double numBelow = a - 1;
+        //at last number
+        if (numBelow == 0 || a == 0) {
+            System.out.println(total);
+            return total; // print final answer :)
+        }
+
+        else {
+            total = total * (a * numBelow);
+            return calculate(numBelow - 1);
+        }
+    }
+}
+
+class calculateAnswer {
+    public static void main(String[] args) {
+        main.calculate(4);
+    }
+}
+
