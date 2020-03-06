86b3511b97c1de60d941fed379135b2fde85e016
Yumibyte <ashraigosa@gmail.com>
2020-03-06 01:11:39 -0800

    initialized dates and added scanner

---
+++ codewarsCalendar/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        String dayName = sc.next();
+        int dayNum = sc.nextInt();
+
+        String[] days = {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+        };
+
+        int position =
+    }
+}
